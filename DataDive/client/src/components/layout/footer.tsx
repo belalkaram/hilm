474b72cfcd63d91042bcd535418956d0f457361b
belalkaram <belalkaram50@gmail.com>
2025-06-26 18:35:20 +0300

    Delete DataDive directory

--- DataDive/client/src/components/layout/footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="text-center py-8 text-dream-secondary text-sm">
-      <p>&copy; 2025 - محلل الأحلام بالذكاء الاصطناعي</p>
-    </footer>
-  );
-}
